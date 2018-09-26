907b88dccbee5ef688d4633d5d5a1bbfa7b427c1
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-26 13:50:28 -0700

    Week 4

---
+++ src/myAges.java
@@ -0,0 +1,9 @@
+
+public class myAges {
+	public static void main(String[] args) {
+		for (int i = 2005; i <=2018; i++) {
+			System.out.println("In "+i+", I was "+(i-2005)+ " years old");
+			
+		}
+	}
+}
